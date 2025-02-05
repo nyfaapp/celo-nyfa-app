f1abeb96a3ee118dbe2857a6ec5b789b17d22d1b
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-02-05 16:00:48 +0300

    remove dummy buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import { Button, HStack } from "@chakra-ui/react";
 export default function Home() {
   return (
     <HStack>
-      <Button>Click me</Button>
-      <Button>Click me</Button>
     </HStack>
   );
 }
