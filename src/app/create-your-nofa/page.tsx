a688cb2054e4f79e213ff7749e986b3a796376b6
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-02-05 16:00:28 +0300

    update: improve nofa creation form

--- src/app/create-your-nofa/page.tsx
+++ src/app/create-your-nofa/page.tsx
@@ -2,11 +2,45 @@
 
 import { BodyLogoCreateYourNoFA } from "@/components/ui/svg-icons/logos/body-logo-create-your-nofa";
 import { BodyLogoNotConnected } from "@/components/ui/svg-icons/logos/body-logo-not-connected";
-import { Button, Text, Flex, Box } from "@chakra-ui/react";
+import {
+  Button,
+  Text,
+  Flex,
+  Box,
+  createListCollection,
+  Stack,
+} from "@chakra-ui/react";
 // import html2canvas from "html2canvas";
 // import { useRef } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Field } from "@/components/ui/field";
+import {
+  SelectContent,
+  SelectItem,
+  SelectRoot,
+  SelectTrigger,
+  SelectValueText,
+} from "@/components/ui/select";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  cryptocurrency: z.string({ message: "Crypto is required" }).array(),
+});
+
+type FormValues = z.infer<typeof formSchema>;
 
 export default function CreateYourNoFA() {
+  const {
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = handleSubmit((data) => console.log(data));
+
   return (
     <>
       <Flex
@@ -16,20 +50,76 @@ export default function CreateYourNoFA() {
         flexDirection={"column"}
         h={"75vh"}
       >
-        <Text color={"#0F1C33"} fontSize={"24px"} fontWeight={"bold"} my={8}>
+        <Text
+          color={"#0F1C33"}
+          fontSize={"24px"}
+          fontWeight={"bold"}
+          my={8}
+          mx={8}
+        >
           Create* your NoFA
         </Text>
         <BodyLogoCreateYourNoFA />
 
-        <Button bgColor={"#FDBB23"} borderRadius={15} mt={12} px={16} w={"3/6"}>
-          <Text color={"#0F1C33"} fontSize={"14px"} fontWeight={"normal"}>
-            Connect
-          </Text>
-        </Button>
+        <Text
+          color={"#0F1C33"}
+          fontSize={"14px"}
+          fontWeight={"normal"}
+          mt={8}
+        >
+          Select and create
+        </Text>
 
-        {/* <Button bgColor={"#FDBB23"} borderRadius={15} mt={12} px={16} w={"3/6"}>
-          <Spinner size="sm" color="default" />
-        </Button> */}
+        <Box my={8}>
+          <form onSubmit={onSubmit}>
+            <Stack gap="4" align="center">
+              <Field
+                invalid={!!errors.cryptocurrency}
+                errorText={errors.cryptocurrency?.message}
+                width="320px"
+              >
+                <Controller
+                  control={control}
+                  name="cryptocurrency"
+                  render={({ field }) => (
+                    <SelectRoot
+                      name={field.name}
+                      value={field.value}
+                      bgColor={"#0F1C33"}
+                      borderRadius={10}
+                      onValueChange={({ value }) => field.onChange(value)}
+                      onInteractOutside={() => field.onBlur()}
+                      collection={cryptocurrencies}
+                    >
+                      <SelectTrigger>
+                        <SelectValueText placeholder="Select crypto" ml={3} />
+                      </SelectTrigger>
+                      <SelectContent>
+                        {cryptocurrencies.items.map((movie) => (
+                          <SelectItem item={movie} key={movie.value}>
+                            {movie.label}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </SelectRoot>
+                  )}
+                />
+              </Field>
+
+              <Button
+                bgColor={"#FDBB23"}
+                borderRadius={15}
+                mt={4}
+                w={"4/6"}
+                type="submit"
+              >
+                <Text color={"#0F1C33"} fontSize={"14px"} fontWeight={"normal"}>
+                  Create
+                </Text>
+              </Button>
+            </Stack>
+          </form>
+        </Box>
       </Flex>
 
       <Box bg="#0F1C33" position="absolute" bottom="0" right="0" py={4}>
@@ -46,3 +136,11 @@ export default function CreateYourNoFA() {
     </>
   );
 }
+
+const cryptocurrencies = createListCollection({
+  items: [
+    { label: "Bitcoin", value: "bitcoin" },
+    { label: "Ethereum", value: "ethereum" },
+    { label: "Ripple", value: "xrp" },
+  ],
+});
