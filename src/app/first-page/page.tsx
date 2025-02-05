8080107600de9c09cd5141e453ebf49af41d2323
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-02-05 20:39:35 +0300

    feature: integrate privy server wallet creation and supabase anon user binding

--- src/app/first-page/page.tsx
+++ src/app/first-page/page.tsx
@@ -1,61 +1,118 @@
 "use client";
 
-import { BodyLogoFirstPage } from "@/components/ui/svg-icons/logos/body-logo-first-page";
+import { BodyLogoFirstPage } from "@/components/svg-icons/logos/body-logo-first-page";
+import { useSupabase } from "@/providers/supabase-provider";
 import { Button, Text, Flex, Box } from "@chakra-ui/react";
+import { useAccount } from "wagmi";
+import { Toaster, toaster } from "@/components/ui/toaster";
+import { useState } from "react";
+import { set } from "react-hook-form";
 import { Spinner } from "@heroui/spinner";
+
 // import html2canvas from "html2canvas";
 // import { useRef } from "react";
 
 export default function FirstPage() {
-  //   const flexRef = useRef(null);
+  const [isCreatingAnonUser, setIsCreatingAnonUser] = useState(false);
 
-  //   const downloadAsPNG = async () => {
-  //     if (flexRef.current) {
-  //       const standardWidth = 450; // Set standard width
-  //       const standardHeight = 900; // Set standard height
-  //       const scale = 2; // Fixed scale instead of device-dependent
+  const { supabase } = useSupabase();
+  const { address } = useAccount();
 
-  //       const canvas = await html2canvas(flexRef.current, {
-  //         scale: scale,
-  //         useCORS: true,
-  //         allowTaint: true,
-  //         logging: false,
-  //         backgroundColor: "#FFFFFF",
-  //         imageTimeout: 0,
-  //         width: standardWidth,
-  //         height: standardHeight,
-  //       });
+  const handleAnonymousSignIn = async () => {
+    setIsCreatingAnonUser(true);
+    try {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
 
-  //       const image = canvas.toDataURL("image/png", 1.0);
+      const privyWalletId: string | null = user?.user_metadata["privyWalletId"];
 
-  //       const finalCanvas = document.createElement("canvas");
-  //       const ctx = finalCanvas.getContext("2d");
-  //       // Set final canvas to our standard size
-  //       finalCanvas.width = standardWidth * scale;
-  //       finalCanvas.height = standardHeight * scale;
+      if (!address) {
+        toaster.create({
+          description: "Connect to Nyfa before getting started.",
+          duration: 3000,
+          type: "info",
+        });
+        setIsCreatingAnonUser(false);
+        return;
+      }
 
-  //       if (ctx) {
-  //         ctx.fillStyle = "#FFFFFF";
-  //         ctx.fillRect(0, 0, finalCanvas.width, finalCanvas.height);
-  //         ctx.imageSmoothingEnabled = true;
-  //         ctx.imageSmoothingQuality = "high";
-  //         ctx.drawImage(canvas, 0, 0);
-  //       }
+      if (user) {
+        toaster.create({
+          description: "Already signed in!",
+          duration: 3000,
+          type: "info",
+        });
+        setIsCreatingAnonUser(false);
 
-  //       const finalImage = finalCanvas.toDataURL("image/png", 1.0);
+        return;
+      } else {
+        const { data, error } = await supabase.auth.signInAnonymously();
+        if (error) throw error;
 
-  //       const link = document.createElement("a");
-  //       const date = new Date();
-  //       link.download = `${date.getTime()}.png`;
-  //       link.href = finalImage;
-  //       link.click();
-  //     }
-  //   };
+        const { data: updatedUser, error: updateError } =
+          await supabase.auth.updateUser({
+            data: { userWalletAddress: address },
+          });
+
+        if (updateError) throw updateError;
+
+        const response = await fetch("/api/wallet", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            privyWalletId: privyWalletId,
+            userWalletAddress: address,
+            authId: updatedUser?.user.id,
+          }),
+        });
+
+        console.log(response);
+
+        const { privyWalletId: _privyWalletId }: { privyWalletId: string } =
+          await response.json();
+
+        const {
+          data: updatedUserAddPrivyWalletId,
+          error: updatedUserAddPrivyWalletIdError,
+        } = await supabase.auth.updateUser({
+          data: { privyWalletId: _privyWalletId },
+        });
+
+        if (updatedUserAddPrivyWalletIdError) throw updatedUserAddPrivyWalletIdError;
+
+        toaster.create({
+          description: "Successfully signed in with wallet!",
+          duration: 3000,
+          type: "success",
+        });
+
+        setIsCreatingAnonUser(false);
+
+        return;
+      }
+
+    } catch (error) {
+      setIsCreatingAnonUser(false);
+
+      toaster.create({
+        description:
+          error instanceof Error ? error.message : "Authentication failed",
+        duration: 3000,
+        type: "error",
+      });
+      console.error("Error:", error);
+
+      return;
+    }
+  };
 
   return (
     <>
+      <Toaster />
       <Flex
-        // ref={flexRef}
         justifyContent={"center"}
         alignItems={"center"}
         flexDirection={"column"}
@@ -70,7 +127,16 @@ export default function FirstPage() {
           not your financial advisor
         </Text>
 
-        <Button bgColor={"#FDBB23"} borderRadius={15} mt={12} px={16} w={"3/6"}>
+        <Button
+          bgColor={"#FDBB23"}
+          borderRadius={15}
+          mt={12}
+          px={16}
+          w={"3/6"}
+          onClick={handleAnonymousSignIn}
+          loading={isCreatingAnonUser}
+          loadingText={<Spinner size="sm" color="default" />}
+        >
           <Text color={"#0F1C33"} fontSize={"14px"} fontWeight={"normal"}>
             Get started
           </Text>
@@ -82,7 +148,13 @@ export default function FirstPage() {
       </Flex>
 
       <Box bg="#0F1C33" position="absolute" bottom="0" right="0" py={4}>
-        <Text color={"white"} fontSize={"14px"} fontWeight={"normal"} mx={16} textAlign={"center"}>
+        <Text
+          color={"white"}
+          fontSize={"14px"}
+          fontWeight={"normal"}
+          mx={16}
+          textAlign={"center"}
+        >
           *nyfa (not her real name) makes it easy for anyone to do their own
           crypto research.
         </Text>
