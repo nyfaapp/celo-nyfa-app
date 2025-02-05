7a7ffccc1cfd1e00b7c52e5af7169f5f516e629b
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-02-05 16:58:29 +0300

    attempt to force dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,9 @@ import { Provider } from "@/components/ui/provider";
 import Header from "@/components/ui/header";
 
 import { Web3Provider } from "@/providers/web3-provider";
-import '@coinbase/onchainkit/styles.css';
-import './onchain-overrides.css';
+import "@coinbase/onchainkit/styles.css";
+import "./onchain-overrides.css";
+import { ColorModeProvider } from "@/components/ui/color-mode";
 
 const parkinsans = Parkinsans({
   subsets: ["latin"],
@@ -23,12 +24,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning className="dark">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${parkinsans.className} bg-white`}>
         <Provider>
           <Web3Provider>
             <Header />
-            {children}
+            <ColorModeProvider forcedTheme="dark">{children}</ColorModeProvider>
           </Web3Provider>
         </Provider>
       </body>
