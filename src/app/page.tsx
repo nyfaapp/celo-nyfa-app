0f9a4dd64ef981e4e8605b86adb0a070dc1afc2b
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-02-05 22:03:21 +0300

    ui: reroute depending, on /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,33 @@
-import { Button, HStack } from "@chakra-ui/react";
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useAccount } from "wagmi";
+import { Flex } from "@chakra-ui/react";
+import { Spinner } from "@heroui/spinner";
 
 export default function Home() {
+  const { isConnected, isDisconnected } = useAccount();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isConnected) {
+      router.replace("/all-nofas");
+    } else if (isDisconnected) {
+      router.replace("/not-connected");
+    }
+  }, [isConnected, isDisconnected, router]);
+
+  // Display a loading state while redirecting
   return (
-    <HStack>
-    </HStack>
+    <Flex
+      // ref={flexRef}
+      justifyContent={"center"}
+      alignItems={"center"}
+      flexDirection={"column"}
+      h={"75vh"}
+    >
+      <Spinner size="sm" color="default" />
+    </Flex>
   );
 }
