4661697029fcf46eb97315e28dbdc281e385d23b
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-02-06 15:46:51 +0300

    remove unneeded code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,30 +8,6 @@ import { Spinner } from "@heroui/spinner";
 import { useSupabase } from "@/providers/supabase-provider";
 
 export default function Home() {
-  const { isConnected, isDisconnected } = useAccount();
-  const router = useRouter();
-  const { supabase } = useSupabase();
-
-  useEffect(() => {
-    const checkUser = async () => {
-      const user = (await supabase.auth.getUser()).data;
-
-      if (isDisconnected) {
-        router.replace("/not-connected");
-      }
-
-      if (isConnected && !user) {
-        router.replace("/first-page");
-      }
-
-      if (isConnected && user) {
-        router.replace("/all-nofas");
-      }
-    };
-    checkUser();
-  }, [isConnected, isDisconnected, router]);
-
-  // Display a loading state while redirecting
   return (
     <Flex
       // ref={flexRef}
