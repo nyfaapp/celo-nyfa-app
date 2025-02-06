23e223308cfb7c4afb53b6148b6b5eaabc066c76
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-02-06 12:03:05 +0300

    update heroui config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@heroui/theme/dist/components/(divider|spinner).js"
+    "./node_modules/@heroui/theme/dist/components/(autocomplete|divider|spinner|button|ripple|form|input|listbox|popover|scroll-shadow).js"
   ],
   theme: {
     extend: {
