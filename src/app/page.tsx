0afcfdc01ed0beee408bc352e054201d2257bf01
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-02-06 18:12:17 +0300

    clean up /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 "use client";
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { useAccount } from "wagmi";
 import { Flex } from "@chakra-ui/react";
 import { Spinner } from "@heroui/spinner";
-import { useSupabase } from "@/providers/supabase-provider";
 
 export default function Home() {
   return (
