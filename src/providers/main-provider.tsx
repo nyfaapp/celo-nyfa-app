e173d347051cc7cec4f9b84cfd7f5c258319dd8a
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-02-06 18:48:38 +0300

    update auth provider

--- src/providers/main-provider.tsx
+++ src/providers/main-provider.tsx
@@ -4,7 +4,7 @@ import { Provider } from "@/components/chakra/ui/provider";
 import { Web3Provider } from "@/providers/web3-provider";
 import { ColorModeProvider } from "@/components/chakra/ui/color-mode";
 import SupabaseProvider from "@/providers/supabase-provider";
-import RedirectProvider from "@/providers/redirect-provider";
+import AuthRedirectProvider from "@/providers/auth-redirect-provider";
 import Header from "@/components/nyfa/header";
 
 export default function MainProvider({
@@ -18,9 +18,9 @@ export default function MainProvider({
         <ColorModeProvider forcedTheme="dark">
           <SupabaseProvider>
             <Header />
-            <RedirectProvider>
+            <AuthRedirectProvider>
               {children}
-            </RedirectProvider>
+            </AuthRedirectProvider>
           </SupabaseProvider>
         </ColorModeProvider>
       </Web3Provider>
