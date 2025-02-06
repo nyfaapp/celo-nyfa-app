b28dab316392a542f4caec7b796200ed9c2fcf87
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-02-06 08:41:37 +0300

    update: improve redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,30 @@ import { useRouter } from "next/navigation";
 import { useAccount } from "wagmi";
 import { Flex } from "@chakra-ui/react";
 import { Spinner } from "@heroui/spinner";
+import { useSupabase } from "@/providers/supabase-provider";
 
 export default function Home() {
   const { isConnected, isDisconnected } = useAccount();
   const router = useRouter();
+  const { supabase } = useSupabase();
 
   useEffect(() => {
-    if (isConnected) {
-      router.replace("/all-nofas");
-    } else if (isDisconnected) {
-      router.replace("/not-connected");
-    }
+    const checkUser = async () => {
+      const user = (await supabase.auth.getUser()).data;
+
+      if (isDisconnected) {
+        router.replace("/not-connected");
+      }
+
+      if (isConnected && !user) {
+        router.replace("/first-page");
+      }
+
+      if (isConnected && user) {
+        router.replace("/all-nofas");
+      }
+    };
+    checkUser();
   }, [isConnected, isDisconnected, router]);
 
   // Display a loading state while redirecting
