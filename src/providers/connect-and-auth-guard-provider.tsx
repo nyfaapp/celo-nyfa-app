94191fd04e383d17862807e9c1dbe5cad381160b
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-02-06 15:45:57 +0300

    improve connect and auth guard

--- src/providers/connect-and-auth-guard-provider.tsx
+++ src/providers/connect-and-auth-guard-provider.tsx
@@ -4,6 +4,8 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useSupabase } from "./supabase-provider";
 import { useAccount } from "wagmi";
+import { Flex } from "@chakra-ui/react";
+import { Spinner } from "@heroui/spinner";
 
 export default function ConnectAndAuthGuardProvider({
   children,
@@ -15,21 +17,31 @@ export default function ConnectAndAuthGuardProvider({
   const { isConnected, isDisconnected } = useAccount();
 
   useEffect(() => {
+    if (user) {
+      router.push("/all-nofas");
+    }
+
     if (isDisconnected) {
       router.push("/not-connected");
     }
 
-    if (isConnected) {
-      if (user) {
-        router.push("/all-nofas");
-      } else {
-        router.push("/first-page");
-      }
+    if (isConnected && !user) {
+      router.push("/first-page");
     }
   }, [user, router, isConnected, isDisconnected]);
 
   if (!user) {
-    return null; // Or a loading spinner
+    return (
+      <Flex
+        // ref={flexRef}
+        justifyContent={"center"}
+        alignItems={"center"}
+        flexDirection={"column"}
+        h={"75vh"}
+      >
+        <Spinner size="sm" color="default" />
+      </Flex>
+    );
   }
 
   return <>{children}</>;
