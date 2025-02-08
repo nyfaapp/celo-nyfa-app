0ba9ffda9d8c76f16f6dbfd7dbd8e61b947b9e82
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-02-08 09:44:28 +0300

    remove props

--- src/components/nyfa/svg-icons/latest-headlines-icon.tsx
+++ src/components/nyfa/svg-icons/latest-headlines-icon.tsx
@@ -1,7 +1,6 @@
-import { IconSvgProps } from "@/types/svgIcon";
 import * as React from "react";
 
-export const LatestHeadlinesIcon: React.FC<IconSvgProps> = ({}) => {
+export const LatestHeadlinesIcon: React.FC = ({}) => {
   return (
     <svg
       width="24"
