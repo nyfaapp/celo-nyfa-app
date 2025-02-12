e4ac6aeb49a264727a3f5c50c943d1e2b296525a
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-02-12 13:17:43 +0300

    update: remove onchainkit wallet impl

--- src/components/chakra/ui/onchainkit-wallet.tsx
+++ src/components/chakra/ui/onchainkit-wallet.tsx
@@ -1,44 +1,44 @@
-"use client";
+// "use client";
 
-import { Box, Text } from "@chakra-ui/react";
-import { Avatar, Name } from "@coinbase/onchainkit/identity";
+// import { Box, Text } from "@chakra-ui/react";
+// import { Avatar, Name } from "@coinbase/onchainkit/identity";
 
-import { Toaster, toaster } from "@/components/chakra/ui/toaster";
+// import { Toaster, toaster } from "@/components/chakra/ui/toaster";
 
-import {
-  ConnectWallet,
-  Wallet,
-  WalletDropdown,
-  WalletDropdownDisconnect,
-} from "@coinbase/onchainkit/wallet";
+// import {
+//   ConnectWallet,
+//   Wallet,
+//   WalletDropdown,
+//   WalletDropdownDisconnect,
+// } from "@coinbase/onchainkit/wallet";
 
-const OnchainKitWallet = () => {
-  return (
-    <>
-      <Toaster />
+// const OnchainKitWallet = () => {
+//   return (
+//     <>
+//       <Toaster />
 
-      <Box>
-        <Wallet>
-          <ConnectWallet
-            text="Connect"
-            onConnect={() => {
-              toaster.create({
-                description: "Connected",
-                duration: 3000,
-                type: "success",
-              });
-            }}
-          >
-            <Avatar className="h-6 w-6" />
-            <Name />
-          </ConnectWallet>
-          <WalletDropdown>
-            <WalletDropdownDisconnect />
-          </WalletDropdown>
-        </Wallet>
-      </Box>
-    </>
-  );
-};
+//       <Box>
+//         <Wallet>
+//           <ConnectWallet
+//             text="Connect"
+//             onConnect={() => {
+//               toaster.create({
+//                 description: "Connected",
+//                 duration: 3000,
+//                 type: "success",
+//               });
+//             }}
+//           >
+//             <Avatar className="h-6 w-6" />
+//             <Name />
+//           </ConnectWallet>
+//           <WalletDropdown>
+//             <WalletDropdownDisconnect />
+//           </WalletDropdown>
+//         </Wallet>
+//       </Box>
+//     </>
+//   );
+// };
 
-export default OnchainKitWallet;
+// export default OnchainKitWallet;
