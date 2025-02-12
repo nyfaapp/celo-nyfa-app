8893db629e5d4a82cae75496c64a99d367246c0c
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-02-12 13:13:07 +0300

    comment out wagmi onchainkit config

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -1,29 +1,29 @@
-import { http, cookieStorage, createConfig, createStorage } from 'wagmi';
-import { baseSepolia } from 'wagmi/chains'; // add baseSepolia for testing
-import { coinbaseWallet } from 'wagmi/connectors';
+// import { http, cookieStorage, createConfig, createStorage } from 'wagmi';
+// import { baseSepolia } from 'wagmi/chains'; // add baseSepolia for testing
+// import { coinbaseWallet } from 'wagmi/connectors';
  
-export function getConfig() {
-  return createConfig({
-    chains: [baseSepolia], // add baseSepolia for testing
-    connectors: [
-      coinbaseWallet({
-        appName: 'OnchainKit',
-        preference: 'all',
-        version: '4',
-      }),
-    ],
-    storage: createStorage({
-      storage: cookieStorage,
-    }),
-    ssr: true,
-    transports: {
-      [baseSepolia.id]: http(), // add baseSepolia for testing
-    },
-  });
-}
+// export function getConfig() {
+//   return createConfig({
+//     chains: [baseSepolia], // add baseSepolia for testing
+//     connectors: [
+//       coinbaseWallet({
+//         appName: 'OnchainKit',
+//         preference: 'all',
+//         version: '4',
+//       }),
+//     ],
+//     storage: createStorage({
+//       storage: cookieStorage,
+//     }),
+//     ssr: true,
+//     transports: {
+//       [baseSepolia.id]: http(), // add baseSepolia for testing
+//     },
+//   });
+// }
  
-declare module 'wagmi' {
-  interface Register {
-    config: ReturnType<typeof getConfig>;
-  }
-}
+// declare module 'wagmi' {
+//   interface Register {
+//     config: ReturnType<typeof getConfig>;
+//   }
+// }
