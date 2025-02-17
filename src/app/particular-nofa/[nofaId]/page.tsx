8d5d2980d477983f02ec22fefb32452d4dcf650e
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-02-17 18:33:30 +0300

    feat: re-enable HeadlineSection component
    Restore headlines display functionality by
    uncommenting HeadlineSection component.

--- src/app/particular-nofa/[nofaId]/page.tsx
+++ src/app/particular-nofa/[nofaId]/page.tsx
@@ -470,7 +470,7 @@ export default function ParticularNoFA() {
             totalSupply={nofa?.totalSupply!}
             circulatingSupply={nofa?.circulatingSupply!}
           />
-          {/* <HeadlineSection headlines={nofa?.headlines!} /> */}
+          <HeadlineSection headlines={nofa?.headlines!} />
           <Footer timeCreated={nofa?.timeCreated!} />
         </Flex>
       </Flex>
